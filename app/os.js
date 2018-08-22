c181625291004d1882b6c3cdce023f1e8a0f745a
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-08-22 15:15:23 +0200

    changed time function

--- app/os.js
+++ app/os.js
@@ -2,13 +2,9 @@ process.stdin.setEncoding('utf-8');
 
 var OSinfo = require('../modules/OSinfo');
 var secToTime = require('../modules/secTotime');
-var reg = new RegExp('^[0-9]+$');
 
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if (reg.test(input)) {
-        secToTime.print(input);
-    }
     if (input !== null) {
         var instruction = input.toString().trim();
         switch (instruction) {
